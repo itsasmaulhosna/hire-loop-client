d1c6f2f0596d5d578550bc776aad2b559219bc3c
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-06-02 23:49:14 +0600

    Implement of drawer sidebar for small and large device

---
+++ src/app/dashboard/recuriter/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecuriterPage = () => {
+    return (
+        <div>
+            <h2 className='text-2xl font-bold mb-4'>Recruiter Dashboard</h2>
+        </div>
+    );
+};
+
+export default RecuriterPage;
