4cd687161d87106bfd59d7619913a6739daa94a1
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-06-03 12:03:30 +0600

    Added neww job form

---
+++ src/app/dashboard/recuriter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecuriterJobs = () => {
+    return (
+        <div>
+          <h2 className="text-4xl">Recruiter Jobs Page</h2>  
+        </div>
+    );
+};
+
+export default RecuriterJobs;
